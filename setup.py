0334117feef44291c32b9fb3cdbe55f98e485cba
Manthan007 <manthansharma7818@gmail.com>
2026-09-04 07:36:02 -0700

    requirements installed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="AI-Medical-Chatbot",
+    version="0.1.0",
+    author="Manthan Sharma",
+    author_email="manthansharma7818@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
